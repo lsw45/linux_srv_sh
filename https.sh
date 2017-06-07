77e8a5167bcf3642af373c6d1b4472fcd8247c88
jry <598821125@qq.com>
2017-06-07 15:58:47 +0800

    update

--- https.sh
+++
@@ -1,9 +0,0 @@
-cd /usr/local/sbin
-wget https://dl.eff.org/certbot-auto
-chmod a+x certbot-auto
-./certbot-auto
-certbot-auto certonly --webroot -w /home/www/lingyun -d lingyun.net -d www.lingyun.net -d git.lingyun.net -d doc.lingyun.net -d bbs.lingyun.net -d u.lingyun.net -d oa.lingyun.net -d zb.lingyun.net -d zp.lingyun.net -d v.lingyun.net -d lyshop.lingyun.net
-
-# 90天会过期用下面的语句更新时间
-cd /usr/local/sbin
-./certbot-auto renew --force-renew
